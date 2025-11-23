54fb383843d028c7da288291999dc40195e3ad1c
Kuliiuu <thedoctorhox@gmail.com>
2025-11-23 13:18:23 +0100

    Initial commit

---
+++ src/main/java/net/kuliiuu/metalsplus/MetalsPlus.java
@@ -0,0 +1,16 @@
+package net.kuliiuu.metalsplus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetalsPlus implements ModInitializer {
+	public static final String MOD_ID = "metalsplus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/kuliiuu/metalsplus/MetalsPlusClient.java
@@ -0,0 +1,11 @@
+package net.kuliiuu.metalsplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MetalsPlusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
