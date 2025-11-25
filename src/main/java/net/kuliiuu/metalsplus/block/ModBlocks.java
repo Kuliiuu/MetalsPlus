06539b03fe3377440e4dffc644ec628902dd25ff
Kuliiuu <thedoctorhox@gmail.com>
2025-11-25 18:53:12 +0100

    removed unused imports

--- src/main/java/net/kuliiuu/metalsplus/block/ModBlocks.java
+++ src/main/java/net/kuliiuu/metalsplus/block/ModBlocks.java
@@ -1,13 +1,11 @@
 package net.kuliiuu.metalsplus.block;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.kuliiuu.metalsplus.MetalsPlus;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
