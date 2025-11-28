f7dd6fb3b759ca8e7deaba6202355942a3af46b5
Kuliiuu <thedoctorhox@gmail.com>
2025-11-28 17:21:49 +0100

    Made bronze look more orange Added more items to the ChiselItem functionality Changed version to alpha3.0.1

--- src/main/java/net/kuliiuu/metalsplus/item/custom/ChiselItem.java
+++ src/main/java/net/kuliiuu/metalsplus/item/custom/ChiselItem.java
@@ -16,15 +16,23 @@ import java.util.Map;
 
 public class ChiselItem extends Item {
     private static final Map<Block, Block> CHISEL_MAP =
-            Map.of(
-                    Blocks.OBSIDIAN, Blocks.CRYING_OBSIDIAN,
-                    Blocks.STONE_BRICKS, Blocks.CRACKED_STONE_BRICKS,
-                    Blocks.DEEPSLATE_BRICKS, Blocks.CRACKED_DEEPSLATE_BRICKS,
-                    Blocks.DEEPSLATE_TILES, Blocks.CRACKED_DEEPSLATE_TILES,
-                    Blocks.NETHER_BRICKS, Blocks.CRACKED_NETHER_BRICKS,
-                    Blocks.POLISHED_BLACKSTONE_BRICKS, Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS,
-                    Blocks.MOSSY_COBBLESTONE, Blocks.COBBLESTONE,
-                    Blocks.MOSSY_STONE_BRICKS, Blocks.STONE_BRICKS
+            Map.ofEntries(
+                    Map.entry(Blocks.OBSIDIAN, Blocks.CRYING_OBSIDIAN),
+                    Map.entry(Blocks.STONE_BRICKS, Blocks.CRACKED_STONE_BRICKS),
+                    Map.entry(Blocks.DEEPSLATE_BRICKS, Blocks.CRACKED_DEEPSLATE_BRICKS),
+                    Map.entry(Blocks.DEEPSLATE_TILES, Blocks.CRACKED_DEEPSLATE_TILES),
+                    Map.entry(Blocks.NETHER_BRICKS, Blocks.CRACKED_NETHER_BRICKS),
+                    Map.entry(Blocks.POLISHED_BLACKSTONE_BRICKS, Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS),
+                    Map.entry(Blocks.MOSSY_COBBLESTONE, Blocks.COBBLESTONE),
+                    Map.entry(Blocks.MOSSY_STONE_BRICKS, Blocks.STONE_BRICKS),
+                    Map.entry(Blocks.MOSSY_COBBLESTONE_SLAB, Blocks.COBBLESTONE_SLAB),
+                    Map.entry(Blocks.MOSSY_COBBLESTONE_STAIRS, Blocks.COBBLESTONE_STAIRS),
+                    Map.entry(Blocks.MOSSY_COBBLESTONE_WALL, Blocks.COBBLESTONE_WALL),
+                    Map.entry(Blocks.MOSSY_STONE_BRICK_SLAB, Blocks.STONE_BRICK_SLAB),
+                    Map.entry(Blocks.MOSSY_STONE_BRICK_STAIRS, Blocks.STONE_BRICK_STAIRS),
+                    Map.entry(Blocks.MOSSY_STONE_BRICK_WALL, Blocks.STONE_BRICK_WALL),
+                    Map.entry(Blocks.INFESTED_MOSSY_STONE_BRICKS, Blocks.INFESTED_STONE_BRICKS),
+                    Map.entry(Blocks.INFESTED_STONE_BRICKS, Blocks.INFESTED_CRACKED_STONE_BRICKS)
             );
 
     public ChiselItem(Settings settings) {
