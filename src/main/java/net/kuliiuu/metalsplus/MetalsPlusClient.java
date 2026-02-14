b9e639acce940bdb2149e2803c03e16fd01b6d42
Marneyy_ <thedoctorhox@gmail.com>
2026-02-14 11:45:29 +0100

    Changed version to 1.4.1

--- src/main/java/net/kuliiuu/metalsplus/MetalsPlusClient.java
+++ src/main/java/net/kuliiuu/metalsplus/MetalsPlusClient.java
@@ -1,16 +1,11 @@
 package net.kuliiuu.metalsplus;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.kuliiuu.metalsplus.block.ModBlocks;
-import net.minecraft.client.render.RenderLayer;
 
 public class MetalsPlusClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLUEBERRY_BUSH, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLACKBERRY_BUSH, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RASPBERRY_BUSH, RenderLayer.getCutout());
+
     }
 }
